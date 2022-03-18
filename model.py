21d5cb26a41a567364974e3727d969fbb94c45b9
Noah Cohen Kalafut <themathmaster3000@gmail.com>
2022-03-18 14:28:05 -0500

    Survey interpretation and underlying feature assumption

--- model.py
+++ model.py
@@ -3,13 +3,13 @@ import torch
 import torch.nn as nn
 
 
-class Model(nn.Module):
-    """Thin, simple NN model"""
-    def __init__(self, num_mentors, num_mentees):
+class EmbeddingGenerationModel(nn.Module):
+    """Thin, simple embedding generation model"""
+    def __init__(self, num_mentors, num_mentees, dim=10):
         super().__init__()
 
-        self.mentors = nn.Embedding(num_mentors, 10)
-        self.mentees = nn.Embedding(num_mentees, 10)
+        self.mentors = nn.Embedding(num_mentors, dim)
+        self.mentees = nn.Embedding(num_mentees, dim)
 
     def forward(self, o_id, e_id):
         """Forward pass for the model"""
@@ -18,3 +18,31 @@ class Model(nn.Module):
         o_norm = torch.norm(o_vec, dim=1)
         e_norm = torch.norm(e_vec, dim=1)
         return torch.sum(o_vec * e_vec, dim=1) / (o_norm * e_norm)
+
+
+class EmbeddingInferenceModel(nn.Module):
+    """Thin, simple embedding inference model"""
+    def __init__(self, input_features, output_features=10):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_features, 2*input_features),
+            nn.BatchNorm1d(2*input_features),
+            nn.LeakyReLU(.1, True),
+
+            nn.Linear(2*input_features, 2*input_features),
+            nn.BatchNorm1d(2*input_features),
+            nn.LeakyReLU(.1, True),
+
+            nn.Linear(2*input_features, input_features),
+            nn.BatchNorm1d(input_features),
+            nn.LeakyReLU(.1, True),
+
+            nn.Linear(input_features, output_features),
+            nn.BatchNorm1d(output_features),
+        )
+
+
+    def forward(self, x):
+        """Forward pass for the model"""
+        return self.layers(x)
