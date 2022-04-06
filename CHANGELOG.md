175a3085f20a4a1b5c8a424b00dd2ab79fcfdcd7
Noah Cohen Kalafut <themathmaster3000@gmail.com>
2022-04-06 16:02:29 -0500

    Adversarial model, revamped sample data

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# 1.0.1
+- Add graph visualization of sampling
+- Add scarcity to sampling (i.e. Mentees usually match only once)
+- Added adversarial implementation
+- Many more visualizations
+- Visualization styling
+
+# 1.0.0
+- Initial implementation
+- Sample generation
+- Embedding generation model
+- Embedding prediction model
--- model.py
+++ model.py
@@ -5,24 +5,24 @@ import torch.nn as nn
 
 class EmbeddingGenerationModel(nn.Module):
     """Thin, simple embedding generation model"""
-    def __init__(self, num_mentors, num_mentees, dim=10):
+    def __init__(self, num_mentees, num_mentors, dim=10):
         super().__init__()
 
-        self.mentors = nn.Embedding(num_mentors, dim)
         self.mentees = nn.Embedding(num_mentees, dim)
+        self.mentors = nn.Embedding(num_mentors, dim)
 
-    def forward(self, o_id, e_id):
+    def forward(self, e_id, o_id):
         """Forward pass for the model"""
-        o_vec = self.mentors(o_id)
         e_vec = self.mentees(e_id)
-        o_norm = torch.norm(o_vec, dim=1)
+        o_vec = self.mentors(o_id)
         e_norm = torch.norm(e_vec, dim=1)
+        o_norm = torch.norm(o_vec, dim=1)
         return torch.sum(o_vec * e_vec, dim=1) / (o_norm * e_norm)
 
 
 class EmbeddingInferenceModel(nn.Module):
     """Thin, simple embedding inference model"""
-    def __init__(self, input_features, output_features=10):
+    def __init__(self, input_features, dim=10):
         super().__init__()
 
         self.layers = nn.Sequential(
@@ -38,8 +38,8 @@ class EmbeddingInferenceModel(nn.Module):
             nn.BatchNorm1d(input_features),
             nn.LeakyReLU(.1, True),
 
-            nn.Linear(input_features, output_features),
-            nn.BatchNorm1d(output_features),
+            nn.Linear(input_features, dim),
+            nn.BatchNorm1d(dim),
         )
 
 
